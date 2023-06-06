723ef33e825fe9152a16726e540e20e2139d4b82
Nidarsh N <nidarsh39@gmail.com>
2023-06-05 23:14:34 -0500

    Added Insertion Sort

--- basics/README.md
+++ basics/README.md
@@ -4,3 +4,4 @@ By Tim Roughgarden
 
 1. Sort  
     1.1 [Selection Sort](/basics/sort/selection_sort.py)
+    1.2 [Insertion Sort](/basics/sort/insertion_sort.py)  
---
+++ basics/sort/insertion_sort.py
@@ -0,0 +1,15 @@
+def sort_insertion(arr, n):
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6]
+    n = len(arr)
+    sort_insertion(arr, n)
+    print(arr)
